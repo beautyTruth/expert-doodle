8eadd3f4b007b8f1604ace72ebeb23950fad39bf
Phil <happy.melodist@gmail.com>
2021-12-23 20:06:04 -0600

    write the calculate password strength and length assessment functions

--- app.js
+++ app.js
@@ -117,21 +117,45 @@ const strengthener = document.querySelector(".strengthener");
 const passwordInput = document.querySelector('input[type="text"]');
 const passwordCheck = document.querySelector(".password-check");
 
-passwordCheck.addEventListener("input", updateStrengthener);
+passwordInput.addEventListener("input", updateStrengthener);
 
 function updateStrengthener() {
   const assessments = calculatePasswordStrength(passwordInput.value);
 
   let strength = 100;
-
   passwordCheck.innerHTML = "";
-  assessments.array.forEach((assessment) => {
+
+  assessments.forEach((assessment) => {
     if (assessment == null) return;
 
     strength -= assessment.strengthLost;
     const pwdCheckEl = document.createElement("p");
-    pwdCheckEl.innerHTML = assessment.pwdCheckEl;
+    pwdCheckEl.innerHTML = assessment.pwdCheck;
     passwordCheck.appendChild(pwdCheckEl);
   });
   strengthener.style.setProperty("--strength-amount", strength);
+}
+
+function calculatePasswordStrength(password) {
+  const assessment = [];
+  assessment.push(lengthAssessment(password));
+  return assessment;
+}
+
+function lengthAssessment(password) {
+  const length = password.length;
+
+  if (length <= 5) {
+    return {
+      pwdCheck: "too short",
+      strengthLost: 40,
+    };
+  }
+
+  if (length <= 10) {
+    return {
+      pwdCheck: "maybe a little longer?",
+      strengthLost: 15,
+    };
+  }
 }
